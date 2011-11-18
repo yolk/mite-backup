ac3c44d2ae3447316edaaf7fda674126259e3a40
Sebastian <sebastian@yo.lk>
2011-11-18 22:01:19 +0100

    Option Hash and some small refactoring

--- lib/mite-backup.rb
+++ lib/mite-backup.rb
@@ -8,15 +8,15 @@ require 'yaml'
 class MiteBackup
   MAX_CHECKS              = 30
   SLEEP_BEFORE_EACH_CHECK = 2 # seconds
-  CONFIG_FILE = File.expand_path('~/.mite-backup.yml')
-  USER_AGENT = "mite-backup/#{MiteBackup::VERSION}"
+  CONFIG_FILE             = File.expand_path('~/.mite-backup.yml')
+  USER_AGENT              = "mite-backup/#{MiteBackup::VERSION}"
 
-  def initialize(account_name, email, password)
-    @account_name = account_name || config["account"]
-    @email        = email        || config["email"]
-    @password     = password     || config["password"]
-    
-    @http = Net::HTTP.new(URI.parse("http://#{@account_name}.mite.yo.lk/").host)
+  def initialize(options={})
+    @options = options
+    self.class.clear_config if options["clear_config"]
+    @account  = options["account"]  || config["account"]
+    @email    = options["email"]    || config["email"]
+    @password = options["password"] || config["password"]
   end
 
   def run
@@ -27,7 +27,7 @@ class MiteBackup
   end
 
   def setup
-    (config["account"] = @account_name) || config.delete("account")
+    (config["account"] = @account) || config.delete("account")
     (config["email"] = @email) || config.delete("email")
     (config["password"] = @password) || config.delete("password")
     
@@ -47,25 +47,26 @@ class MiteBackup
   private
 
     def runnable?
-      failed "Please provide your account name with --account [ACCOUNT]." unless @account_name
+      failed "Please provide your account name with --account [ACCOUNT]." unless @account
       failed "Please provide your mite.users email with --email [EMAIL]." unless @email
       failed "Please provide your mite.users password with --password [PASSWORD]." unless @password
     end
 
     def create
-      @id = perform_request(Net::HTTP::Post.new("/account/backup.json"))["id"]
+      @id = parse_json(perform_request(Net::HTTP::Post.new("/account/backup.json")))["id"]
     end
 
     def check
       MAX_CHECKS.times do |i|
         sleep(SLEEP_BEFORE_EACH_CHECK)
-        break if @ready = perform_request(Net::HTTP::Get.new("/account/backup/#{@id}.json"))["ready"]
+        @ready = parse_json(perform_request(Net::HTTP::Get.new("/account/backup/#{@id}.json")))["ready"]
+        break if @ready
       end
     end
 
     def download
       if @ready
-        content = perform_request(Net::HTTP::Get.new("/account/backup/#{@id}/download.json"), false).body
+        content = perform_request(Net::HTTP::Get.new("/account/backup/#{@id}/download.json"))
         gz = Zlib::GzipReader.new(StringIO.new(content), :external_encoding => content.encoding)
         puts gz.read
       else
@@ -73,18 +74,26 @@ class MiteBackup
       end
     end
 
-    def perform_request(request, json=true)
+    def perform_request(request)
       request.basic_auth(@email, @password)
       request['User-Agent'] = USER_AGENT
-      response = @http.request(request)
+      response = mite.request(request)
       if response.code == "401"
         failed "Could not authenticate with email #{@email.inspect} and provided password. The user needs to be an admin or the owner of the mite.account!"
       elsif !["200", "201"].include?(response.code)
         failed "mite responded with irregular code #{response.code}"
       end
-      json ? MultiJson.decode(response.body)["backup"] : response
+      response.body
     end
-  
+
+    def parse_json(string)
+      MultiJson.decode(string)["backup"]
+    end
+
+    def mite
+      @mite ||= Net::HTTP.new(URI.parse("http://#{@account}.mite.yo.lk/").host)
+    end
+
     def failed(reason)
       $stderr.puts "Failed: #{reason}"
       exit(1)
